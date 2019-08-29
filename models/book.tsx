823f75007973066659eb12d13f616b25f20fd7ae
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-29 17:09:18 -0400

    book model

--- models/book.tsx
+++ models/book.tsx
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: { type: String, unique: true, required: true },
+    authors: [{ type: String, required: true }],
+    description: String,
+    image: String,
+    link: { type: String, unique: true, required: true },
+  });
+  
+  const Book = mongoose.model("Book", bookSchema);
+  
+  module.exports = Book;
