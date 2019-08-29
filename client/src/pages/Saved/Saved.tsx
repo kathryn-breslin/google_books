08c1d095f524131c2d27fb43ba09805fbac4af04
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-29 17:33:25 -0400

    paths between search and saved pages

--- client/src/pages/Saved/Saved.tsx
+++ client/src/pages/Saved/Saved.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Saved () {
     return (
         <div>
+            <Link to="/"><h1>Search</h1></Link>
             <h1>Saved Page</h1>
         </div>
     );
--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Search extends Component {
     render() {
         return (
             <div>
+                <Link to="/saved"><h1>Saved</h1></Link>
                 <h1>Search Page</h1>
             </div>
         )
