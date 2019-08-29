37c84e27f3c4594af8e4f6da1f1a4ebb69dfe7f9
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-29 17:52:30 -0400

    api routes

--- routes/api/books.tsx
+++ routes/api/books.tsx
@@ -1,9 +1,12 @@
+export {};
 const router = require("express").Router();
-const booksController = require('../../controllers/bookController');
+const booksController = require("../../controllers/bookController");
 
-router.route('/api/books')
-    .get(booksController.findAll)
-    .post(booksController.create);
+router
+  .route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
 
-router.route('/api/books/:id')
-    .delete(booksController.remove);
+router.route("/:id").delete(booksController.remove);
+
+module.exports = router;
