69e132595eecddeb99dc666e106d0ce682ae44ba
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-29 18:16:58 -0400

    setting up search functions

--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -3,6 +3,30 @@ import { Link } from "react-router-dom";
 import API from "../../utils/API";
 
 class Search extends Component {
+    state = {
+        result: {}, 
+        search: ""
+    }
+
+    searchBooks = (query: string) => {
+        API.search(query)
+        .then(res => this.setState({ result: res.data }))
+        .catch(err => console.log(err));
+    }
+
+    handleInputChange = (event: any) => {
+        const value = event.target.value;
+        const name = event.target.value;
+
+        this.setState({ [name]: value });
+    }
+
+    handleFormSearch = (event: any) => {
+        const { search } = this.state;
+        event.preventDefault();
+        this.searchBooks(search);
+    }
+
     render() {
         return (
             <div>
